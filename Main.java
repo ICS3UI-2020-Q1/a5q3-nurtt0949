0ba85db74a2b1af3e437acf8c9a2b76d04bf13a0
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-06 11:00:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *tells user their factorial
+ * @author Tolga Nurtekin 
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a Scanner
+     Scanner input = new Scanner(System.in);
+    //ask user for number they want the factorial of
+     System.out.println("Please enter an integer to calculate the factorial of");
+    //declare a variable to store users integer
+     int integer = input.nextInt();
+    //declare a variable for accumalator
+     int total = 1;
+    //create a for loop
+     for(int count = 1; count <= integer; count++){
+    //multiplictation for the total
+       total = total * count;
+    
+     }
+     //print the factorial to the screen
+     System.out.print(integer + "! " + "= " + total);
+
+
     
   }
 }
+
